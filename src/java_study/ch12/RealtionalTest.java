235690c6f9241e282af500354c01c2fc14b4afec
LeeSeunghyeon <5644718@naver.com>
2022-08-09 22:25:44 +0900

    initial Commit

---
+++ src/java_study/ch12/RealtionalTest.java
@@ -0,0 +1,24 @@
+package java_study.ch12;
+
+public class RealtionalTest {
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 3;
+
+        boolean value = (num1 > num2); //True
+        System.out.println(value);
+
+        System.out.println(num1 < num2); //false
+        System.out.println(num1 >= num2); //true
+        System.out.println(num1 <= num2); //false
+        System.out.println(num1 == num2); //false
+        System.out.println(num1 != num2); //true
+
+
+    }
+}
+/*
+관계연산자: 이항연산자. 연산자가 true(참), 거짓(false)으로 반환. 비교연산자. 조건문 반복문의 조건식에 사용
+
+*/
