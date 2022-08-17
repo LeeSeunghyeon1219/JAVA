3e66bd4bf66c22526c719acc9012bddbc44657c8
LeeSeunghyeon <5644718@naver.com>
2022-08-17 22:59:33 +0900

    initial Commit

---
+++ src/java_study/chap2/ch02/Order.java
@@ -0,0 +1,11 @@
+package java_study.chap2.ch02;
+
+public class Order {
+
+    int orderId;
+    String buyerId;
+    String sellerId;
+    int productId;
+    String orderDate;
+}
+
---
+++ src/java_study/chap2/ch02/UserInfo.java
@@ -0,0 +1,11 @@
+package java_study.chap2.ch02;
+
+public class UserInfo {
+
+    String userId;
+    String userPassWord;
+    String userName;
+    String userAddress;
+    int phoneNumber;
+}
+
